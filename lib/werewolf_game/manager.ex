13e3185b2cc030781a70f4cb2968c1e8100c6565
scorsi <sylvain.corsini@protonmail.com>
2020-05-06 01:42:41 +0200

    Remove Ets package to use native erlang ets

--- lib/werewolf_game/manager.ex
+++ lib/werewolf_game/manager.ex
@@ -9,17 +9,14 @@ defmodule WerewolfGame.Manager do
 
   @impl true
   def init(state) do
-    Process.flag(:trap_exit, true)
-
-    {:ok, table} =
-      Ets.Set.new(ordered: true, read_concurrency: true, compressed: false, protection: :private)
+    table = :ets.new(:werewolf_game_room, [:set, :private])
 
     {:ok, Map.put(state, :table, table)}
   end
 
   @impl true
   def terminate(_reason, state) do
-    Ets.Set.delete(state.table)
+    :ets.delete(state.table)
     state
   end
 
@@ -43,36 +40,33 @@ defmodule WerewolfGame.Manager do
   def handle_call({:list_rooms}, _from, state),
     do: {:reply, list_rooms(state.table), state}
 
+  @spec get_room(String.t()) :: {:ok, Room} | {:error, any}
   def get_room(room_name),
     do: GenServer.call(__MODULE__, {:get_room, room_name})
 
   defp get_room(table, room_name) do
-    case Ets.Set.get(table, room_name) do
-      {:ok, nil} -> {:error, :no_room_found}
-      {:ok, room} -> {:ok, deserialize(room)}
-      error -> error
+    case :ets.lookup(table, room_name) do
+      [] -> {:error, :no_room_found}
+      [room] -> {:ok, deserialize(room)}
     end
   end
 
+  @spec create_room(Room) :: {:ok, Room} | {:error, any}
   def create_room(room),
     do: GenServer.call(__MODULE__, {:create_room, room})
 
   defp create_room(table, %Room{name: room_name} = room) do
-    case get_room(table, room_name) do
-      {:error, :no_room_found} ->
-        case Ets.Set.put_new(table, serialize(room)) do
-          {:ok, _} -> {:ok, room}
-          error -> error
-        end
-
-      {:ok, _} ->
-        {:error, :room_already_exists}
-
-      error ->
-        error
+    if String.length(room_name) == 0 do
+      {:error, :invalid_room_name}
+    else
+      case :ets.insert_new(table, serialize(room)) do
+        true -> {:ok, room}
+        false -> {:error, :room_already_exists}
+      end
     end
   end
 
+  @spec update_room(String.t(), Room) :: {:ok, Room} | {:error, any}
   def update_room(room_name, room),
     do: GenServer.call(__MODULE__, {:update_room, room_name, room})
 
@@ -85,37 +79,36 @@ defmodule WerewolfGame.Manager do
         end
 
       false ->
-        case Ets.Set.put(table, serialize(room)) do
-          {:ok, _} -> {:ok, room}
-          error -> error
-        end
+        :ets.insert(table, serialize(room))
+        {:ok, room}
     end
   end
 
+  @spec delete_room(String.t()) :: {:ok, Room} | {:error, any}
   def delete_room(room_name),
     do: GenServer.call(__MODULE__, {:delete_room, room_name})
 
   defp delete_room(table, room_name) do
     case get_room(table, room_name) do
       {:ok, room} ->
-        case Ets.Set.delete(table, room_name) do
-          {:ok, _} -> {:ok, room}
-          error -> error
-        end
+        :ets.delete(table, room_name)
+        {:ok, room}
 
       error ->
         error
     end
   end
 
+  @spec list_rooms() :: {:ok, [Room]} | {:error, any}
   def list_rooms(),
     do: GenServer.call(__MODULE__, {:list_rooms})
 
   defp list_rooms(table) do
-    case Ets.Set.to_list(table) do
-      {:ok, rooms} -> {:ok, Enum.map(rooms, &deserialize(&1))}
-      error -> error
-    end
+    {
+      :ok,
+      :ets.tab2list(table)
+      |> Enum.map(&deserialize(&1))
+    }
   end
 
   defp serialize(%Room{name: name, members: members, owner: owner}) do
