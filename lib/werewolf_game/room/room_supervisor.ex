5cce9539c66190cfcf2da5cbadfe651899653d34
scorsi <sylvain.corsini@protonmail.com>
2020-05-11 15:39:15 +0200

    Update the RoomSupervisor to better handle Rooms

--- lib/werewolf_game/room/room_supervisor.ex
+++ lib/werewolf_game/room/room_supervisor.ex
@@ -1,34 +1,66 @@
 defmodule WerewolfGame.RoomSupervisor do
   use DynamicSupervisor
 
-  alias WerewolfGame.RoomRegistry
+  alias WerewolfGame.Room
 
   def start_link(init_arg) do
     DynamicSupervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
   end
 
-  def start_room(room) do
+  def start_room(%Room{} = room) do
     DynamicSupervisor.start_child(
       __MODULE__,
-      {WerewolfGame.Room, initial_room: room, name: RoomRegistry.get_process_name(room.id)}
+      {WerewolfGame.Room, initial_state: room, name: get_process_via(room)}
     )
-
     room
   end
 
   def kill_room(room) do
-    case Registry.lookup(WerewolfGame.RoomRegistry, room.id) do
-      [] ->
-        room
-
-      [{pid, _}] ->
-        DynamicSupervisor.terminate_child(__MODULE__, pid)
-        room
+    case lookup_process(room) do
+      nil -> :ok
+      pid -> DynamicSupervisor.terminate_child(__MODULE__, pid)
     end
+    room
   end
 
   @impl true
   def init(_init_arg) do
     DynamicSupervisor.init(strategy: :one_for_one)
+  end
+
+  def get_process_via(%Room{} = room) do
+    {:via, Registry, {WerewolfGame.RoomRegistry, "room:#{room.id}"}}
+  end
+
+  def get_process_via(room_id) do
+    {:via, Registry, {WerewolfGame.RoomRegistry, "room:#{room_id}"}}
+  end
+
+  def lookup_process(%Room{} = room) do
+    case Registry.lookup(WerewolfGame.RoomRegistry, "room:#{room.id}") do
+      [] -> nil
+      [{pid, _}] -> pid
+    end
+  end
+
+  def lookup_process(room_id) do
+    case Registry.lookup(WerewolfGame.RoomRegistry, "room:#{room_id}") do
+      [] -> nil
+      [{pid, _}] -> pid
+    end
+  end
+
+  def call_process(room, message) do
+    case lookup_process(room) do
+      nil -> nil
+      pid -> GenServer.call(pid, message)
+    end
+  end
+
+  def cast_process(room, message) do
+    case lookup_process(room) do
+      pid -> GenServer.cast(pid, message)
+    end
+    room
   end
 end
