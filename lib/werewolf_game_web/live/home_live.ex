7eb208ceddb8e5b26e98205cdc9037f3eb973858
scorsi <sylvain.corsini@protonmail.com>
2020-05-11 15:40:02 +0200

    Add a title to the HomeLive page

--- lib/werewolf_game_web/live/home_live.ex
+++ lib/werewolf_game_web/live/home_live.ex
@@ -13,30 +13,63 @@ defmodule WerewolfGameWeb.HomeLive do
   def mount(_params, session, socket) do
     PublicRoomAgent.subscribe()
     socket = maybe_assign_current_user(socket, session)
-    {:ok, assign(socket, rooms: PublicRoomAgent.list_rooms())}
+    {:ok, assign(socket, rooms: PublicRoomAgent.list_rooms(), page_title: "Accueil")}
   end
 
   @impl true
   def handle_info(
         %{event: :actualized_room, payload: room},
-        %{assigns: %{rooms: rooms}} = socket
+        %{
+          assigns: %{
+            rooms: rooms
+          }
+        } = socket
       ) do
-    {:noreply, assign(socket, rooms: rooms |> Map.put(room.id, room))}
+    {
+      :noreply,
+      assign(
+        socket,
+        rooms: rooms
+               |> Map.put(room.id, room)
+      )
+    }
   end
 
   @impl true
   def handle_info(
         %{event: :registered_room, payload: room},
-        %{assigns: %{rooms: rooms}} = socket
+        %{
+          assigns: %{
+            rooms: rooms
+          }
+        } = socket
       ) do
-    {:noreply, assign(socket, rooms: rooms |> Map.put(room.id, room))}
+    {
+      :noreply,
+      assign(
+        socket,
+        rooms: rooms
+               |> Map.put(room.id, room)
+      )
+    }
   end
 
   @impl true
   def handle_info(
         %{event: :unregistered_room, payload: room},
-        %{assigns: %{rooms: rooms}} = socket
+        %{
+          assigns: %{
+            rooms: rooms
+          }
+        } = socket
       ) do
-    {:noreply, assign(socket, rooms: rooms |> Map.delete(room.id))}
+    {
+      :noreply,
+      assign(
+        socket,
+        rooms: rooms
+               |> Map.delete(room.id)
+      )
+    }
   end
 end
