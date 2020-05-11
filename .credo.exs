a6a5ed990e5cbd0e37c6eb37615eed29861cb57c
scorsi <sylvain.corsini@protonmail.com>
2020-05-11 18:11:11 +0200

    Change credo config

--- .credo.exs
+++ .credo.exs
@@ -11,8 +11,6 @@
         {Credo.Check.Design.AliasUsage, priority: :low},
         {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 120},
         {Credo.Check.Readability.ParenthesesOnZeroArityDefs, parens: true},
-        {Credo.Check.Design.TagTODO, exit_status: 2},
-        {Credo.Check.Design.TagFIXME, false},
         {Credo.Check.Refactor.MapInto, false},
         {Credo.Check.Warning.LazyLogging, false},
       ]
